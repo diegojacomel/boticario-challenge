e94c03daf64801ea665c17ef5838a2d1ae95615d
Diego Jacomel <diego@bornlogic.com>
2019-11-29 17:11:58 -0300

    Add container

---
+++ src/containers/PurchaseRegister/PurchaseRegister.js
@@ -0,0 +1,12 @@
+// Modules
+import React from 'react';
+
+function PurchaseRegister() {
+    return (
+        <div>
+            Purchase Register
+        </div>
+    )
+}
+
+export default PurchaseRegister;
